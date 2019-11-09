0621f41187585c4e519c6c8ea53691585bf86c8b
Gustavo Castillo <gcdcoder@gmail.com>
2019-11-09 14:49:47 -0600

    Update jest and TS config files to coverage and better import paths

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,30 @@
+const path = require('path');
+
 module.exports = {
   verbose: true,
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    }
+  },
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    '!**/src/helpers/*.ts',
+    '!**/src/dataStructures/*.ts'
+  ],
+  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: [
+    '<rootDir>',
+    'node_modules',
+    path.join(__dirname, 'src')
+  ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!@foo)'
+  ]
 };
