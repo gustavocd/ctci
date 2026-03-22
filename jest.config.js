1335b24634c2b55fa03d162c4cddca00732fa70d
Gustavo Castillo <gcdcoder@gmail.com>
2026-03-21 22:52:00 -0600

    chore: update configuration files for Bun migration and add Prettier support

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,24 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   verbose: true,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
   globals: {
     'ts-jest': {
       tsConfig: './__tests__/tsconfig.json',
-    }
+    },
   },
   collectCoverageFrom: [
     '**/src/**/*.ts',
     '!**/src/helpers/*.ts',
-    '!**/src/dataStructures/*.ts'
+    '!**/src/dataStructures/*.ts',
   ],
   moduleFileExtensions: ['ts', 'js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  moduleDirectories: [
-    '<rootDir>',
-    'node_modules',
-    path.join(__dirname, 'src')
-  ],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!@foo)'
-  ]
-};
+  moduleDirectories: ['<rootDir>', 'node_modules', path.join(__dirname, 'src')],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!@foo)'],
+}
